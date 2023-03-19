e1732d42e120d02fb14417eca77694669c888ed1
Ceren Şimşek <emresimsek321@gmail.com>
2023-03-19 15:29:52 +0300

    reset_scoreboard function changed

--- Snake_game/score.py
+++ Snake_game/score.py
@@ -4,11 +4,12 @@ FONT = ("courier", 20, "normal")
 class Score(Turtle):
 	def __init__(self):
 		super().__init__()
+		self.high_score_file = open("high_score.txt", "r+")
 		self.penup()
 		self.hideturtle()
 		self.color("white")
 		self.goto(x=0, y=270)
-		self.high_score = 0
+		self.high_score = int(self.high_score_file.read())
 		self.score = 0
 		self.write_score()
 
@@ -25,3 +26,6 @@ class Score(Turtle):
 		self.score = 0
 		self.clear()
 		self.write_score()
+		self.high_score_file.seek(0)
+		self.high_score_file.truncate()
+		self.high_score_file.write(str(self.high_score))
