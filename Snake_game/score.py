1d22619372e3a4849542bb928896447066b809dd
Ceren Şimşek <emresimsek321@gmail.com>
2023-03-19 04:09:59 +0300

    High score added

--- Snake_game/score.py
+++ Snake_game/score.py
@@ -8,17 +8,20 @@ class Score(Turtle):
 		self.hideturtle()
 		self.color("white")
 		self.goto(x=0, y=270)
+		self.high_score = 0
 		self.score = 0
-		self.write(arg=f"Score: {self.score}", align="center", font=FONT)
+		self.write_score()
 
 	def update_score(self):
 		self.clear()
 		self.score += 1
+		if self.score > self.high_score:
+			self.high_score = self.score
 
 	def write_score(self):
-		self.update_score()
-		self.write(f"Score: {self.score}", align="center", font=FONT)
+		self.write(f"Score: {self.score}     High Score :{self.high_score}", align="center", font=FONT)
 
-	def game_over(self):
-		self.goto(x=0, y=0)
-		self.write(arg="Game Over", align="center", font=FONT)
+	def reset_scoreboard(self):
+		self.score = 0
+		self.clear()
+		self.write_score()
